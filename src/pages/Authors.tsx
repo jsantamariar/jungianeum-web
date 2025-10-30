4841ba695674624a4e1ade283c12b6b4c52f85ec
Jorge Santamaria <santamaria.web.dev@gmail.com>
2025-10-30 16:30:33 -0500

    refactor: remove padding from main content and optimize authors component rendering

--- src/pages/Authors.tsx
+++ src/pages/Authors.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import { authorsByLetter } from "../constants/authors";
 
 function Authors() {
@@ -7,17 +8,15 @@ function Authors() {
         <div className="authors-grid">
           <div className="authors-grid-divider" />
           {Object.entries(authorsByLetter).map(([letter, authors], idx) => (
-            <>
-              <div key={letter} className="authors-letter-section">
+            <Fragment key={letter}>
+              <div className="authors-letter-section">
                 <h2 className="authors-letter">{letter}</h2>
                 {authors.length > 0 ? (
                   <ul className="authors-list">
-                    {authors.map((author, index) => (
-                      <>
-                        <li key={index} className="author-name">
-                          {author}
-                        </li>
-                      </>
+                    {authors.map((author) => (
+                      <li key={author} className="author-name">
+                        {author}
+                      </li>
                     ))}
                   </ul>
                 ) : (
@@ -25,7 +24,7 @@ function Authors() {
                 )}
               </div>
               {(idx + 1) % 3 === 0 && <div className="authors-grid-divider" />}
-            </>
+            </Fragment>
           ))}
         </div>
       </div>
